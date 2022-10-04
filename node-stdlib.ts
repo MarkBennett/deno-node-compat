8274861bb31674e72e9720c8ceecad4566f2d504
Mark Bennett <mark@burmis.ca>
2022-10-04 12:49:46 -0600

    Add an example using node stdlibs

--- node-stdlib.ts
+++ node-stdlib.ts
@@ -1,1 +1,7 @@
+// node-stdlib.ts
 import { readFileSync } from "https://deno.land/std@0.158.0/node/fs.ts";
+import { stdout } from "https://deno.land/std@0.158.0/node/process.ts";
+
+const data = readFileSync("hello.txt", "utf8");
+
+stdout.write(data);
